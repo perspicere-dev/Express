418d497ff745d7a85b2bce596a16a5fa0ec7b51c
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-11-27 13:33:34 +0100

    add middleware use

--- server.js
+++ server.js
@@ -3,26 +3,33 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
-});
-
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/about.html'));
-});
-
-app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/contact.html'));
-});
-
-app.get('/info', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/info.html'));
-});
-
-app.get('/history', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/history.html'));
-});
-
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
-});
+app.use((req, res, next) => {
+    res.show = (name) => {
+      res.sendFile(path.join(__dirname, `/views/${name}`));
+    };
+    next();
+  });
+  
+  app.get('/', (req, res) => {
+    res.show('index.html');
+  });
+  
+  app.get('/about', (req, res) => {
+    res.show('about.html');
+  });
+  
+  app.get('/contact', (req, res) => {
+    res.show('contact.html');
+  });
+  
+  app.get('/info', (req, res) => {
+    res.show('info.html');
+  });
+  
+  app.get('/history', (req, res) => {
+    res.show('history.html');
+  });
+  
+  app.listen(8000, () => {
+    console.log('Server is running on port: 8000');
+  });
