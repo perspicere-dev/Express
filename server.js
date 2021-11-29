953d72080f92d72ee07aebb5c9b54ae8dbc9b0a1
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-11-29 18:13:31 +0100

    test commit

--- server.js
+++ server.js
@@ -1,35 +1,41 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-    res.show = (name) => {
-      res.sendFile(path.join(__dirname, `/views/${name}`));
-    };
-    next();
-  });
-  
-  app.get('/', (req, res) => {
-    res.show('index.html');
-  });
-  
-  app.get('/about', (req, res) => {
-    res.show('about.html');
-  });
-  
-  app.get('/contact', (req, res) => {
-    res.show('contact.html');
-  });
-  
-  app.get('/info', (req, res) => {
-    res.show('info.html');
-  });
-  
-  app.get('/history', (req, res) => {
-    res.show('history.html');
-  });
-  
-  app.listen(8000, () => {
-    console.log('Server is running on port: 8000');
-  });
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+  res.render('index', { layout: false });
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { layout: false }); //sasa
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { layout: false });
+});
+
+app.get('/info', (req, res) => {
+  res.render('info', { layout: false });
+});
+
+app.get('/history', (req, res) => {
+  res.render('history', { layout: false });
+});
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+})
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
