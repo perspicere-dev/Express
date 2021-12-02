7e0e7497dae1df65305598d62ae7811ee38a245e
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-12-02 19:45:25 +0100

    finish form images. Traials with dispaling the image

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer  = require('multer');
+const upload = multer({ dest: './public/uploads/' })
 
 const app = express();
 app.engine('hbs', hbs());
@@ -8,6 +10,8 @@ app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: false }));
+
+const cpUpload = upload.fields([{ name: 'author'}, { name: 'sender'}, { name: 'title'}, { name: 'projectDesign'}, { name: 'message'}, { name: 'title'}])
 
 app.get(['/', '/home'], (req, res) => {
   res.render('index');
@@ -33,16 +37,24 @@ app.get('/history', (req, res) => {
   res.render('history', { layout: 'dark'});
 });
 
-app.post('/contact/send-message', (req, res) => {
+app.post('/contact/send-message', cpUpload, (req, res) => {
+  let picName; 
+//   let picture; 
+//   console.log(req.files)
+
+  if (req.files.projectDesign) {
+    picName = req.files.projectDesign[0].originalname;
+    // picture = req.files.projectDesign[0].path;
+  }
 
   const { author, sender, title, message } = req.body;
 
-  if(author && sender && title && message) {
-    res.render('contact', { isSent: true });
-  }
-  else {
-    res.render('contact', { isError: true });
-  }
+if(author && sender && title && message && picName) {
+  res.render('contact', { isSent: true, picNameOther: picName }); //removed picture: picture
+}
+else {
+  res.render('contact', { isError: true });
+}
 
 });
 
